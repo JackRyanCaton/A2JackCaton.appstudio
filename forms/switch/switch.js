99e9b131941e34e0583d7ddc3b4d702e7a2b2c9f
JackRyanCaton <jrc88858@creighton.edu>
2021-09-19 22:17:49 -0500

    update

--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -1,21 +1,17 @@
 // variables declared in If/Else form
 
 // Switch
-/*
+
 switch(stateAbrev) {
   case "NE":
     if (userTemp < 32)
-      console.log(`${firstName}), ${messages[0]}`);
+      console.log(`${firstName}, ${messages[0]}`);
     else if (userTemp >=32 && userTemp <= 50)
-      console.log(`${firstName}), ${messages[1]}`)
-  break;
+      console.log(`${firstName}, ${messages[1]}`)
+    break
   case "FL":
     if (userTemp >= 32 && userTemp <= 50)
-      console.log(`${firstName}), ${messages[2]}`);
+      console.log(`${firstName}, ${messages[2]}`);
     else if (userTemp > 50 && userTemp <= 70)
-      console.log(`${firstName}), ${messages[3]}`)
-    break;
-  default:
-    console.log("Try again..");
-    break;
-*/
+      console.log(`${firstName}, ${messages[3]}`)
+}
